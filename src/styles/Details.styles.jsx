10375d65c1fd02cb3aee437c10b41d2d0003fd4c
Okan <okanmerx@gmail.com>
2024-08-01 16:39:19 -0700

    Update-recipe

--- src/styles/Details.styles.jsx
+++ src/styles/Details.styles.jsx
@@ -5,7 +5,7 @@ export const DetailsContainer = styled.div`
   z-index: -1;
   margin-top: 100px;
   width: 100%;
-  padding: 1rem;
+  padding: 2rem;
   h3 {
     text-align: center;
   }
@@ -18,7 +18,7 @@ export const DetailsContainer = styled.div`
     }
   }
 
-  #nutrients-card{
+  #nutrients-card {
     margin-top: 1rem;
     @media screen and (max-width: ${({ theme }) => theme.breakpoint}) {
       margin: 0;
@@ -36,38 +36,36 @@ export const StyledImage = styled.img`
 `;
 
 export const Title = styled.h2`
-margin-bottom: 0.5rem;
-text-align: center;
-color: ${({theme})=> theme.palette.primary.main};
-`
+  margin-bottom: 0.5rem;
+  text-align: center;
+  color: ${({ theme }) => theme.palette.primary.main};
+`;
 
 export const List = styled.ul`
-margin-top:0.5rem;
-font-size: 18px;
-li{
+  margin-top: 0.5rem;
+  font-size: 18px;
+  li {
     margin-bottom: 0.1rem;
-    text-align:left;
-}
-`
-
+    text-align: left;
+  }
+`;
 
 export const Section = styled.section`
-width:30%;
-margin-top: 1rem;
-@media screen and (max-width: ${({ theme }) => theme.breakpoint}) {
+  width: 30%;
+  margin-top: 1rem;
+  @media screen and (max-width: ${({ theme }) => theme.breakpoint}) {
     width: 100%;
-}
+  }
 `;
 
 export const Button = styled(Link)`
-
-padding:  0.5rem 1rem;
-color: white;
-background-color: ${({theme})=> theme.palette.primary.main};
-font-size: 1rem;
-border: none;
-border-radius: 5px;
-margin-top: 1rem;
-display: inline-block;
-cursor: pointer;
+  padding: 0.5rem 1rem;
+  color: white;
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  font-size: 1rem;
+  border: none;
+  border-radius: 5px;
+  margin-top: 1rem;
+  display: inline-block;
+  cursor: pointer;
 `;
